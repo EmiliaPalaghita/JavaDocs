9d3f4512027fb6d3e9068b2bc9bf69cecf94df24
EmiliaPalaghita <p.emilia13@yahoo.com>
2017-07-05 16:55:27 +0300

    Day 2 Java - Collections

---
+++ Collections/warmup-part1/src/main/java/exercise/exercise4/IndexOutOfBounds.java
@@ -0,0 +1,10 @@
+package exercise.exercise4;
+
+/**
+ * Created by Emilia.Palaghita on 7/5/2017.
+ */
+public class IndexOutOfBounds extends RuntimeException {
+    public IndexOutOfBounds() {
+        System.out.println("Index out of bounds exception");
+    }
+}
